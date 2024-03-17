355b2ee28985c724db31bb283b541f6b31c7c878
siri <sureshaa.2628@gmail.com>
2024-03-17 18:38:39 +0530

    usage of id loctors

---
+++ usageofid.java
@@ -0,0 +1,23 @@
+package WebElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UsageOfId {
+public static void main(String[] args) {
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.facebook.com/");
+	WebElement emailTextField = driver.findElement(By.id("email"));
+	emailTextField.sendKeys("Qspders");
+	WebElement passwordTextField = driver.findElement(By.id("pass"));
+	passwordTextField.sendKeys("123");
+	WebElement loginButton = driver.findElement(By.name("login"));
+	loginButton.click();
+	
+	
+	
+}
+}
